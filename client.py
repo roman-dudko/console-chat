3c708d1652144a9ef25fc441f60eb1581429ae79
Roman Dudko <romandudko@touchsoft.by>
2020-09-13 16:45:21 +0300

    fixed #9 #3 #11

--- client.py
+++ client.py
@@ -1,30 +1,40 @@
 import socket
-import threading
 import os
+from threading import Thread
 
-nickname = input("Choose your nickname: ")
+server = input("Enter server (press Enter to use default): ") or "127.0.1.1"
+port = int(input("Enter port (press Enter to use default): ") or "7777")
+nickname = input("Enter nickname: ")
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(('127.0.1.1', 7777))
+client.connect((server, port))
 os.system('cls' if os.name == 'nt' else 'clear')
+
+shutdown = False
 
 
 def listen_server():
     client.send(nickname.encode('utf-8'))
     while True:
-        message = client.recv(1024).decode('utf-8')
-        print(message)
+        received_message = client.recv(1024).decode('utf-8')
+        if received_message:
+            print(received_message)
 
 
-def send_msg():
-    while True:
-        message = '{}: {}'.format(nickname, input(''))
-        print('\033[F\033[K', end="")  # clear last line with entered message
-        client.send(message.encode('utf-8'))
+if __name__ == '__main__':
+    listen_thread = Thread(target=listen_server, daemon=True)
+    listen_thread.start()
 
+    while not shutdown:
+        try:
+            send_message = '{}: {}'.format(nickname, input(''))
+            print('\033[F\033[K', end="")  # clear last line with entered message
+            client.send(send_message.encode('utf-8'))
+        except KeyboardInterrupt:
+            shutdown = True
+            client.send(f"{nickname} exited from server".encode('utf-8'))
+        except BrokenPipeError:
+            shutdown = True
+            print("Server is not available!")
 
-listen_thread = threading.Thread(target=listen_server)
-listen_thread.start()
-
-send_thread = threading.Thread(target=send_msg)  # ToDo: may be thread is not required
-send_thread.start()
+    client.close()
--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import socket
 from threading import Thread
+import time
 
 host = socket.gethostbyname(socket.gethostname())
 port = 7777
@@ -7,10 +8,11 @@ port = 7777
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((host, port))
 server.listen()
-print(f"[ Listening for connections on {host}:{port}... ]")
+print(f"Listening for connections at {host}:{port}...")
 
 clients = []
 nicknames = []
+server_shutdown = False
 
 
 def broadcast(message):
@@ -19,24 +21,36 @@ def broadcast(message):
         client.send(message)
 
 
-def listen_client(client):
+def listen_client(client, nickname):
     """Handle client messages, process commands, broadcast messages"""
-    while True:
+    connected = True
+    while connected:
         message = client.recv(1024)
-        broadcast(message)
+
+        if message.decode('utf-8') == f"{nickname} exited from server":
+            nicknames.remove(nickname)
+            clients.remove(client)
+            print(message.decode('utf-8'))
+            connected = False
+        time_now = time.strftime("%H.%M.%S", time.localtime())
+        message_formatted = f"[{time_now}] {message.decode('utf-8')}"
+        print(message_formatted)
+        broadcast(message_formatted.encode('utf-8'))
 
 
-def start_server():
-    """Handle clients on separate threads"""
-    while True:
-        client, address = server.accept()
-        nickname = client.recv(1024).decode('utf-8')
-        print(f"[ Accepted new connection from {address}, username: {nickname} ]")
-        nicknames.append(nickname)
-        clients.append(client)
-        client.send('Welcome to server!'.encode('utf-8'))
-        thread = Thread(target=listen_client, args=(client,))
-        thread.start()
-
-
-start_server()
+if __name__ == '__main__':
+    while not server_shutdown:
+        try:
+            client, address = server.accept()
+            nickname = client.recv(1024).decode('utf-8')
+            time_now = time.strftime("%H.%M.%S", time.localtime())
+            print(f"[{time_now}] Accepted new connection from {address}, username: {nickname} ")
+            nicknames.append(nickname)
+            clients.append(client)
+            client.send('Welcome to server!'.encode('utf-8'))
+            thread = Thread(target=listen_client, args=(client, nickname,), daemon=True)
+            thread.start()
+        except KeyboardInterrupt:
+            server_shutdown = True
+            print('Server shut down!')
+            broadcast('Server shut down!'.encode('utf-8'))
